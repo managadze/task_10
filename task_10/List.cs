1269a78c407e6256ffa86bdf98c65f8e028b3329
Алина Манагадзе <agmanagadze_1@edu.hse.ru>
2019-06-12 00:27:25 +0500

    Готово

---
+++ task_10/List.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_10
+{
+    public class Node
+    {
+        public int data;
+        public Node next;
+
+        public Node()
+        {
+            data = 0;
+            next = null;
+        }
+
+        public Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+
+        public override string ToString()
+        {
+            return data.ToString() + " ";
+        }
+    }
+
+    public class List
+    {
+        private Node head;
+
+        public Node Create(int n)
+        {
+            head = new Node(1);
+            Node temp = head;
+            int i = 2;
+            while (i <= n)
+            {
+                temp=temp.next = new Node(i++);
+            }
+            temp.next = head;
+            return head;
+        }
+
+        public int Delete(int m)
+        {
+            if ((head!=head.next))
+            {
+                int m1 = m - 1;
+                int i = 1;
+                while (i < m1)
+                {
+                    i++;
+                    head = head.next;
+                }
+                head=head.next = head.next.next;
+                this.Delete(m);
+            }
+            return head.data;
+        }
+    }
+}
--- task_10/Program.cs
+++ task_10/Program.cs
@@ -8,8 +8,53 @@ namespace task_10
 {
     class Program
     {
+        public static void Color(string txt)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(txt);
+            Console.ResetColor();
+        }
+
+        public static void Error(string txt)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine(txt);
+            Console.ResetColor();
+        }
+        public static int Enter()
+        {
+            int el = 0;
+            bool ok = true;
+            do
+            {
+                try
+                {
+                    el = Int32.Parse(Console.ReadLine());
+                    ok = true;
+                    if (el < 1)
+                    {
+                        Error("Ошибка, число должно быть больше 0");
+                        ok = false;
+                    }
+                }
+                catch
+                {
+                    ok = false;
+                    Error("Ошибка, введите целое число");
+                }
+            } while (!ok);
+            return el;
+        }
         static void Main(string[] args)
         {
+            List l = new List();
+            Color("Введите количество человек");
+            int n = Enter();
+            l.Create(n);
+            Color("Введите номер, который нужно будет удалить");
+            int m = Enter();
+            Color("Номер оставшегося человека = "+l.Delete(m));
+            Console.ReadKey();
         }
     }
 }
